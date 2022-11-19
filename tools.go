ebdac932d21ec862561b45933fa2b66996748f0c
Hayden Young <hayden@hbjy.dev>
2022-11-19 14:03:57 +0000

    feat: install golangci-lint

--- tools.go
+++ tools.go
@@ -1,9 +1,10 @@
+//go:build tools
 // +build tools
 
 package main
 
 import (
-  _ "github.com/spf13/cobra-cli"
-  _ "github.com/deepmap/oapi-codegen/cmd/oapi-codegen"
+	_ "github.com/deepmap/oapi-codegen/cmd/oapi-codegen"
+	_ "github.com/golangci/golangci-lint/cmd/golangci-lint"
+	_ "github.com/spf13/cobra-cli"
 )
-
